78b62730236a554f69e27e424739f00ecec3aa6f
Nathan Pham <nathanpham.me@gmail.com>
2021-12-04 23:30:44 -0800

    atoms support className

--- components/Atoms/Button/PrimaryButton.js
+++ components/Atoms/Button/PrimaryButton.js
@@ -1,8 +1,8 @@
-export default function PrimaryButton({ children }) {
+export default function PrimaryButton({ className, children }) {
 
     return (
 
-        <button className="bg-black text-white rounded-md px-6 py-3 w-44 font-semibold border transition-colors border-black hover:bg-white hover:text-black">{ children }</button>
+        <button className={ [ "bg-black text-white rounded-md px-6 py-3 w-44 font-semibold border transition-colors border-black hover:bg-white hover:text-black", className ].join(' ') }>{ children }</button>
 
     )
 
