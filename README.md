84f1be32a96331277e6d8ca9391543fc7a4f7bbd
Nathan Pham <nathanpham.me@gmail.com>
2022-02-07 16:24:32 -0800

    footer socials

--- README.md
+++ README.md
@@ -2,13 +2,8 @@
 https://deloro.farm
 
 ## TODO
-- [x] Refactor Header Component
-- [x] Enhance SEO with Root Component
 - [ ] Farm Tour
-- [x] Add "Edit" button to posts
 - [ ] Add functionality to forms (Contact Form, Icons)
-- [x] Hover states for posts
-- [x] Add photos
 - [ ] Loader?
 - [ ] Responsive design
 - [ ] Gallery of Images
--- components/Atoms/Footer.js
+++ components/Atoms/Footer.js
@@ -1,11 +1,14 @@
 export default function Footer() {
+    const Link = ({ href, children }) => (
+        <a href={href} target="_blank" rel="noreferrer" className="hover:text-black text-gray-500 transition-color font-xl">
+            {children}
+        </a>
+    );
 
+    // prettier-ignore
     return (
-
-        <footer className="py-12 text-center text-gray-500 font-xl">
-            Rising from the rubble. Del Oro FFA.
+        <footer className="py-12 text-center">
+            <Link href="https://www.instagram.com/deloroffa_">Instagram</Link> • <Link href="https://www.facebook.com/deloroloomisffa">Facebook</Link> • <Link href="https://www.remind.com/join/deloroffa">Remind</Link>
         </footer>
-
-    )
-
+    );
 }
