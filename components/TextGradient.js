31616c19921f63c4ce224f584224530288345ec6
Nathan Pham <nathanpham.me@gmail.com>
2022-03-12 15:58:40 -0800

    safari gradient fix???

--- components/TextGradient.js
+++ components/TextGradient.js
@@ -1,9 +1,24 @@
-export default function TextGradient({ gradient="from-yellow-400 via-red-400 to-blue-500", children }) {
-
+export default function TextGradient({
+    gradient = "from-yellow-400 via-red-400 to-blue-500",
+    children,
+}) {
     return (
-        
-        <span className={ [ "text-transparent bg-clip-text bg-gradient-to-r", gradient ].join(' ') }>{ children }</span>
-
-    )
-
+        <>
+            <span
+                className={[
+                    "text-transparent bg-clip-text bg-gradient-to-r",
+                    gradient,
+                ].join(" ")}
+            >
+                {children}
+            </span>
+            <style jsx>{`
+                span {
+                    -webkit-box-decoration-break: clone;
+                    -webkit-background-clip: text;
+                    -webkit-text-fill-color: transparent;
+                }
+            `}</style>
+        </>
+    );
 }
