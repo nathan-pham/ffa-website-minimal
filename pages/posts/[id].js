b79f251bbc741b1d4603871e73cd4f6ef6970ab3
Nathan Pham <nathanpham.me@gmail.com>
2022-03-14 12:33:01 -0700

    better post design

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,6 @@
 import Section from "@components/Sections/Section";
 import TextGradient from "@components/TextGradient";
 import Footer from "@components/Atoms/Footer";
-import Header from "@components/Atoms/Header";
 
 import Link from "next/link";
 
@@ -20,33 +19,24 @@ const postsDirectory = path.join(process.cwd(), "./pages/posts/markdown");
 export default function Post({ metadata, content, id }) {
     const title = regexifyString({
         pattern: /<.*?>/gim,
-        decorator: (match, index) => (
-            <TextGradient key={index}>{match.slice(1, -1)}</TextGradient>
-        ),
+        decorator: (match, index) => <TextGradient key={index}>{match.slice(1, -1)}</TextGradient>,
         input: metadata.title,
     });
 
     return (
         <Root title={metadata.title}>
             <Section>
-                <h1 className="text-5xl md:text-6xl font-extrabold leading-tight">
-                    {title}
-                </h1>
-                <div className="mt-2">
+                <h1 className="text-5xl md:text-6xl font-extrabold">{title}</h1>
+                <div className="mt-4">
                     <Link href="/posts">
                         <a className="hover:underline">← Back to Posts</a>
                     </Link>{" "}
                     •{" "}
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href={`https://github.com/nathan-pham/ffa-website-minimal/blob/main/pages/posts/markdown/${id}.md`}
-                        className="hover:underline cursor-pointer"
-                    >
+                    <a target="_blank" rel="noreferrer" href={`https://github.com/nathan-pham/ffa-website-minimal/blob/main/pages/posts/markdown/${id}.md`} className="hover:underline cursor-pointer">
                         Edit
                     </a>
                 </div>
-                <div className="prose prose-slate w-full mt-4">
+                <div className="prose prose-slate w-full mt-8">
                     <div dangerouslySetInnerHTML={{ __html: content }} />
                 </div>
             </Section>
@@ -74,9 +64,7 @@ export async function getStaticProps({ params }) {
     const fileContents = await fs.readFile(fullPath, "utf8");
     const matterResult = matter(fileContents);
 
-    const processedContent = (
-        await remark().use(html).process(matterResult.content)
-    ).toString();
+    const processedContent = (await remark().use(html).process(matterResult.content)).toString();
 
     return {
         props: {
