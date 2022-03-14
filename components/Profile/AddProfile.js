4e9291321f963dc2806848f4f8df45a524e1c8d3
Nathan Pham <nathanpham.me@gmail.com>
2022-03-14 12:44:49 -0700

    sponsorship and officer application link

--- components/Profile/AddProfile.js
+++ components/Profile/AddProfile.js
@@ -1,10 +1,12 @@
-import { Add } from "react-ionicons"
+import { Add } from "react-ionicons";
 
 export default function AddProfile() {
     return (
-        <div className="border rounded-lg h-full flex flex-col items-center justify-center cursor-pointer text-center transition-all hover:shadow-lg hover:-translate-y-1">
-            <Add className="scale-150" />
-            <p className="max-w-[80%] text-gray-500 mt-6 leading-snug">This could be you! Apply to our officer team.</p>
-        </div>
-    )
+        <a href="https://docs.google.com/document/d/1ZsDzzRJYmbAlRdW3CRQocIrlTQejT502d3ov7RJ24Jc/edit">
+            <div className="border rounded-lg h-full flex flex-col items-center justify-center cursor-pointer text-center transition-all hover:shadow-lg hover:-translate-y-1">
+                <Add className="scale-150" />
+                <p className="max-w-[80%] text-gray-500 mt-6 leading-snug">This could be you! Apply to our officer team.</p>
+            </div>
+        </a>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -24,30 +24,21 @@ export default function Home() {
                         Agriculture <TextGradient>Reimagined</TextGradient>
                     </h1>
                     <p className="mt-6 mb-8 text-gray-500 text-lg md:text-xl max-w-2xl mx-auto">
-                        Del Oro FFA is a youth organization that provides the
-                        best learning experience with an obsessive focus on
-                        leadership and career skills.
+                        Del Oro FFA is a youth organization that provides the best learning experience with an obsessive focus on leadership and career skills.
                     </p>
                 </div>
             </Section>
 
             <Section>
                 <div className="text-center">
-                    <p className="uppercase font-mono text-sm font-semibold tracking-widest my-8">
-                        Explore the Del Oro FFA Way
-                    </p>
+                    <p className="uppercase font-mono text-sm font-semibold tracking-widest my-8">Explore the Del Oro FFA Way</p>
 
                     <Step number={1} />
 
                     <h2 className="my-6 text-5xl font-bold">
-                        Learn by{" "}
-                        <TextGradient gradient="from-blue-500 to-green-300">
-                            doing stuff.
-                        </TextGradient>
+                        Learn by <TextGradient gradient="from-blue-500 to-green-300">doing stuff.</TextGradient>
                     </h2>
-                    <p className="text-gray-500 text-xl">
-                        Agricultural education includes 3 components.
-                    </p>
+                    <p className="text-gray-500 text-xl">Agricultural education includes 3 components.</p>
                 </div>
 
                 <div className="mt-16">
@@ -65,22 +56,13 @@ export default function Home() {
 
             <Section className="mt-10">
                 <div className="text-center">
-                    <Step
-                        number={2}
-                        circleGradient="from-orange-500 to-yellow-300"
-                        lineGradient="from-orange-500"
-                    />
+                    <Step number={2} circleGradient="from-orange-500 to-yellow-300" lineGradient="from-orange-500" />
 
                     <h2 className="my-6 text-5xl font-bold">
-                        Work with our{" "}
-                        <TextGradient gradient="from-orange-500 to-yellow-300">
-                            awesome team.
-                        </TextGradient>
+                        Work with our <TextGradient gradient="from-orange-500 to-yellow-300">awesome team.</TextGradient>
                     </h2>
                     <p className="text-gray-500 max-w-2xl text-xl mx-auto">
-                        Doing stuff alone is lame. Collaborate with your peers
-                        and our officers to make events happen, participate in
-                        national competitions, and have fun!
+                        Doing stuff alone is lame. Collaborate with your peers and our officers to make events happen, participate in national competitions, and have fun!
                     </p>
                 </div>
 
@@ -99,37 +81,20 @@ export default function Home() {
 
             <Section className="mt-10">
                 <div className="text-center">
-                    <Step
-                        number={3}
-                        circleGradient="from-purple-500 to-pink-400"
-                        lineGradient="from-purple-500"
-                    />
+                    <Step number={3} circleGradient="from-purple-500 to-pink-400" lineGradient="from-purple-500" />
 
                     <h2 className="my-6 text-5xl font-bold">
-                        Trusted by{" "}
-                        <TextGradient gradient="from-purple-500 to-pink-400">
-                            amazing businesses.
-                        </TextGradient>
+                        Trusted by <TextGradient gradient="from-purple-500 to-pink-400">amazing businesses.</TextGradient>
                     </h2>
-                    <p className="text-gray-500 text-xl">
-                        You're in good hands. Promise.
-                    </p>
+                    <p className="text-gray-500 text-xl">You're in good hands. Promise.</p>
 
                     <div className="flex justify-center mt-10">
-                        <PrimaryButton>Sponsor Us</PrimaryButton>
+                        <a href="/sponsors.pdf">
+                            <PrimaryButton>Sponsor Us</PrimaryButton>
+                        </a>
                     </div>
                 </div>
             </Section>
-
-            {/* <Section className="mt-20">
-                <div className="text-center">
-                    <p className="uppercase font-mono text-sm font-semibold tracking-widest my-4">
-                        Begin your FFA journey
-                    </p>
-                </div>
-            </Section>
-            <RegisterForm variant="footer" />
-            */}
             <Footer />
         </Root>
     );
